05f621c70cece81025bee14f287ebc68c33fb499
Fred Heuermann <fred.heuermann@uni-oldenburg.de>
2026-02-06 15:04:26 +0100

    finished first two tasks, continue tomorrow

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,1 +1,20 @@
-#test
+		# print header
+echo -e "FASTA File Statistics:\n----------------------"
+		
+
+		# assess number of sequences:
+num_sequences=$(grep '>' $1 | wc -l)
+echo "Number of sequences: $num_sequences"
+
+
+		# assess total length of sequences
+len_sequences=$(grep -v '>' $1 | wc -c)
+len_sequences=$((len_sequences-$(grep -v '>' $1 | wc -l)))
+		# substracts num_lines in sequences. Gets rid of linebreak-bias.
+echo "Total length of sequences: $len_sequences"
+
+
+#Length of the longest sequence: ______
+#Length of the shortest sequence: ______
+#Average sequence length: ______
+#GC Content (%): ______
