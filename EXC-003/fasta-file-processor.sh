f3aa21ad53cef9777d4f29395883ff96ee812579
Fred Heuermann <fred.heuermann@uni-oldenburg.de>
2026-02-07 14:42:01 +0100

    Found an alternative, better solution for task #2 🦄

--- EXC-003/fasta-file-processor.sh
+++ EXC-003/fasta-file-processor.sh
@@ -1,17 +1,19 @@
 		# print header
 echo -e "FASTA File Statistics:\n----------------------"
-		
+		# store file in variable
+fasta=$1
 
 		# assess number of sequences:
-num_sequences=$(grep '>' $1 | wc -l)
+num_sequences=$(grep '>' $fasta | wc -l)
 echo "Number of sequences: $num_sequences"
 
 
-		# assess total length of sequences
-len_sequences=$(grep -v '>' $1 | wc -c)
-len_sequences=$((len_sequences-$(grep -v '>' $1 | wc -l)))
-		# substracts num_lines in sequences. Gets rid of linebreak-bias.
-echo "Total length of sequences: $len_sequences"
+		# assess total length of sequences -
+		# first solution involved "wc -c" but that was inferior
+		# alternative: grep -v '>' -> for loop -> count if [ATGC]
+just_sequences=$(grep -v '>' $fasta)
+just_sequences="${just_sequences//[^ATGC]}"
+echo "Total length of sequences: ${#just_sequences}"
 
 
 #Length of the longest sequence: ______
