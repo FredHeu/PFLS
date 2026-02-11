9fb8d431bf136091820583d63f480a4da333f28a
Fred Heuermann <fred.heuermann@uni-oldenburg.de>
2026-02-11 17:42:45 +0100

    Nearly everything done. Remaining: Defline manipulation and copy checkm and gtdb file.

--- EXC-004/generate-combined-data.py
+++ EXC-004/generate-combined-data.py
@@ -1,5 +1,6 @@
 # setup
-import os, shutil
+import os, shutil, re
+
 path = "./EXC-004/RAW-DATA"
 destination = "./EXC-004/COMBINED-DATA"
 # create directory or remove contents
@@ -19,21 +20,44 @@ with os.scandir(path) as it:
 
 # extracts ID from sample-translation -> stored in xxx
 for directory in dir_list:
-   DNA_path = path + "/" + directory + "/bins"   
+   DNA_path = path + "/" + directory + "/bins"
+   zMAG = 1
+   zBIN = 1   
    with open('./EXC-004/RAW-DATA/sample-translation.txt', 'r') as f:
       for line in f.readlines():
          if directory in line:
             sample_translation = line.split()
             xxx = sample_translation[1]
 
-# copy unbined to combined dir:
+# copy unbinned to combined dir:
    shutil.copy(DNA_path + '/bin-unbinned.fasta', destination + '/' + xxx + '_UNBINNED.fa')
 
 
 # assess yyy - therefore: extract bin# from file name -> search from checkm.txt
+# while at it, iterate seperatly for zzz, copy and rename files into output folder
 
-   # returns xxx_and_name for each FASTA
-#   with os.scandir(DNA_path) as it:
-#      for entry in it:
-#         if not entry.name.startswith('.') and entry.is_file():
-#            print(xxx + '_' + entry.name)
+   filelist = [f for f in os.listdir('./EXC-004/RAW-DATA/' + directory + '/bins')]
+   for file in filelist:
+      if not file.startswith('.') and not file == "bin-unbinned.fasta":
+         yyybin = file.rstrip('.fasta')
+         with open('./EXC-004/RAW-DATA/' + directory + '/checkm.txt', 'r') as f:
+            for line in f.readlines():
+               if yyybin in line:
+                  checkm = line.split()
+                  compl = float(checkm[12])
+                  conta = float(checkm[13])
+                  if compl > 50 and conta < 5:
+                     yyy = "MAG"
+                     zzzMAG = str(zMAG).zfill(3)
+                     zMAG += 1
+                     shutil.copy(DNA_path + '/' + file, destination + '/' + xxx + '_' + yyy + '_' + zzzMAG + '.fa')
+                  else:
+                     yyy = "BIN"
+                     zzzBIN = str(zBIN).zfill(3)
+                     zBIN += 1
+                     shutil.copy(DNA_path + '/' + file, destination + '/' + xxx + '_' + yyy + '_' + zzzBIN + '.fa')
+
+
+# Tasks remaining:
+#  Manipulate Defline with xxx
+#  copy and rename checkm.txt and gtdb file
